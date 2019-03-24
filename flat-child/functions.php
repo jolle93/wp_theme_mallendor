064a2fc59b7707c8829e25d0ed45310869d766dd
Julian Paul <revenpaul@web.de>
2019-03-24 12:16:59 +0100

    removed powered by

--- flat-child/functions.php
+++ flat-child/functions.php
@@ -6,4 +6,5 @@ function md_enqueue_styles() {
 	$parten_style = 'flat-style';
 
 	wp_enqueue_style($parten_style, get_template_directory_uri() . '/style.css');
+	wp_enqueue_style($parten_style, get_template_directory_uri() . '/assets/css/overrides.css');
 }
